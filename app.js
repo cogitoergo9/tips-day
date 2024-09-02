0f85e298c0d9604b666cf9f01e43bdeae39b1a1e
GABRIEL ENRIQUE CASTILLO GONZALEZ <gabcast@mx1.ibm.com>
2024-09-02 16:37:28 -0600

    fix script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = new express();
-const port = 8080;
+const port = 10000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
